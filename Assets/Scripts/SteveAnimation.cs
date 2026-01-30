55ed913eedc710aa9ad6677d09a60e84f77236d3
caballero\robert <robertc13883@gmail.com>
2026-01-30 04:05:35 -0500

    Was able to do the entire animation!!!

--- Assets/Scripts/SteveAnimation.cs
+++ Assets/Scripts/SteveAnimation.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class SteveAnimation : MonoBehaviour
 {
+
+    public Rigidbody2D rigidBody;
+    public Animator animator;
+
+    private Vector2 input;
+    public float speed = 2f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+
+        animator.SetFloat("Horizontal", input.x);
+        animator.SetFloat("Vertical", input.y);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.linearVelocity = input * speed;
     }
 }
